de0c57f8eae6c8b88a5b4347a78a4313ea06f8f7
Daniel Műller <daniel.muller1918@seznam.cz>
2023-02-26 11:00:39 +0100

    initialized tRPC Client and preparation for forms pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next/types";
+import Header from "~/components/Header";
+
+const Home: NextPage = () => {
+  
+    return (
+      <>
+        <Header />
+      </>
+    );
+  };
+  
+  export default Home;
---
+++ src/pages/registration.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next/types";
+import Header from "~/components/Header";
+
+const Registration: NextPage = () => {
+    return (
+      <>
+        <Header />
+        
+      </>
+    );
+  };
+  
+  export default Registration;
