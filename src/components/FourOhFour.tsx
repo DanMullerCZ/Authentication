524fe5376e8efd9f61ad369a9cb179b1203a01ca
Daniel Műller <daniel.muller1918@seznam.cz>
2023-02-26 10:46:30 +0100

    404 page created

---
+++ src/components/FourOhFour.tsx
@@ -0,0 +1,12 @@
+import { type NextComponentType } from "next";
+
+
+const FourOhFour_COMPONENT: NextComponentType = () => {
+    return (
+        <div>
+            Page not found
+        </div>
+    )
+}
+
+export default FourOhFour_COMPONENT
---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import FourOhFour_COMPONENT from "~/components/FourOhFour";
+
+const FourOhFour: NextPage = () => {
+    return (
+        <FourOhFour_COMPONENT />
+    )
+}
+
+export default FourOhFour
